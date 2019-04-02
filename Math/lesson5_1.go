81e71c0c6f5d5dfacfc15b261ac6bdd737b2afb4
Mingfei Ding <xiesansi@gmail.com>
2019-04-03 00:58:53 +0800

    save

---
+++ Math/lesson5_1.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+}
+
+func lesson5_1(totalReword int) result []int{
+
+	rewards := []int{1, 2, 5, 10}
+	
+	if totalReword < 0 {
+		return 
+	}
+	
+	if totalReword == 0 {
+		fmt.Println(result)
+		return result
+	}
+
+
+
+	for _, reward := range rewards {
+		
+	}
+
+}
