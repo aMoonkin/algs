6cafab587599a0b3dcfff0848ce4a15b30a388ea
Mingfei Ding <xiesansi@gmail.com>
2019-04-04 23:40:38 +0800

    save

--- Math/lesson5_1.go
+++ Math/lesson5_1.go
@@ -3,26 +3,25 @@ package main
 import "fmt"
 
 func main() {
-
+	result := make([]int, 0)
+	lesson5_1(10, result)
 }
 
-func lesson5_1(totalReword int) result []int{
+func lesson5_1(totalReword int, result []int) {
 
 	rewards := []int{1, 2, 5, 10}
-	
+
 	if totalReword < 0 {
-		return 
+		return
 	}
-	
+
 	if totalReword == 0 {
 		fmt.Println(result)
-		return result
 	}
 
-
-
 	for _, reward := range rewards {
-		
+		newResult := append(result, reward)
+		lesson5_1(totalReword-reward, newResult)
 	}
 
 }
